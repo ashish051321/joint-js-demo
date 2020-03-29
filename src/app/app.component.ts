ee899d37cf5c7a18a040d35dd5691923a42fa26c
ashish051321 <ashish.25jl@gmail.com>
2020-03-29 18:07:55 +0530

    initial setp of jointjs and basic rendering of connected nodes done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+import * as jQuery from 'jquery';
+import * as _ from 'lodash';
+import * as $ from 'backbone';
+import * as joint from "../../node_modules/jointjs/dist/joint.js";
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +12,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'joint-js-demo';
+
+  ngOnInit() {
+    let graph = new joint.dia.Graph;
+
+    let paper = new joint.dia.Paper({
+      el: jQuery("#paper"),
+      width: 600,
+      height: 200,
+      model: graph,
+      gridSize: 1
+    });
+
+    let rect = new joint.shapes.basic.Rect({
+      position: { x: 100, y: 30 },
+      size: { width: 100, height: 30 },
+      attrs: { rect: { fill: 'blue' }, text: { text: 'my box', fill: 'white' } }
+    });
+
+    let rect2 = rect.clone() as joint.shapes.basic.Rect;
+    rect2.translate(300);
+
+    var link = new joint.dia.Link({
+      source: { id: rect.id },
+      target: { id: rect2.id }
+    });
+
+    graph.addCells([rect, rect2, link]);
+}
+
 }
